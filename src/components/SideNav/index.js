85390a54816011a9c28d7df397a1435556c9b867
Luciana Castro <lucianaycastro@gmail.com>
2019-01-16 23:59:19 -0300

    Fixing category title.

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -10,29 +10,29 @@ class SideNav extends Component {
   render() {
     return (
       <div className='sticky-wrapper'>
-      <StickyBox offsetTop={20} offsetBottom={20} bottom={false}>
-        <div className='side-nav-wrapper'>
-          <Scrollspy className='side-nav' items={data.map(category => category.title)} currentClassName="is-current">
+        <StickyBox offsetTop={20} offsetBottom={20} bottom={false}>
+          <div className='side-nav-wrapper'>
+            <Scrollspy className='side-nav' items={data.map(category => category.title)} currentClassName="is-current">
 
-            {data.map((category, index) => (
-              <li key={category + index}><a href={'#' + category.title} type='category' >{category.title}</a>
-                <Scrollspy items={category.data.map(subcategory => subcategory.title)} currentClassName="is-current">
-                  {category.data.map((subcategory, index) => (
-                    <li key={subcategory + index}><a href={'#' + subcategory.title} type='subcategory' >{subcategory.title}</a>
-                      <Scrollspy items={subcategory.data.map(section => section.title)} currentClassName="is-current">
-                        {subcategory.data.map((section, index) => (
-                          <li><a href={'#' + section.title} key={section + index} data={section.data} >{section.title}</a></li>
-                        ))}
-                      </Scrollspy>
-                    </li>
-                  ))}
-                </Scrollspy>
-              </li>
-            ))
-            }
-          </Scrollspy>
-        </div>
-      </StickyBox>
+              {data.map((category, index) => (
+                <li key={category + index}><a className='item-title' href={'#' + category.title} type='category' >{category.title}</a>
+                  <Scrollspy items={category.data.map(subcategory => subcategory.title)} currentClassName="is-current">
+                    {category.data.map((subcategory, index) => (
+                      <li key={subcategory + index}><a className='item-title' href={'#' + subcategory.title} type='subcategory' >{subcategory.title}</a>
+                        <Scrollspy items={subcategory.data.map(section => section.title)} currentClassName="is-current">
+                          {subcategory.data.map((section, index) => (
+                            <li><a className='item-title' href={'#' + section.title} key={section + index} data={section.data} >{section.title}</a></li>
+                          ))}
+                        </Scrollspy>
+                      </li>
+                    ))}
+                  </Scrollspy>
+                </li>
+              ))
+              }
+            </Scrollspy>
+          </div>
+        </StickyBox>
       </div>
     )
   }
