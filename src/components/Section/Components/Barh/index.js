bf104d1b450cfce1d66d794ff2699903f30022dd
Luciana Castro <lucianaycastro@gmail.com>
2019-02-05 00:34:14 -0300

    Styiling tooltip in Bahr component.

--- src/components/Section/Components/Barh/index.js
+++ src/components/Section/Components/Barh/index.js
@@ -20,7 +20,7 @@ const COLORS = [
   '#9265b3',
   '#f15b88',
   '#fea241',
-  
+
   // https://www.color-hex.com/color-palette/67140
   '#8577b6',
   '#d072cc',
@@ -64,7 +64,6 @@ class Barh extends Component {
     isPercentual: PropTypes.bool,
     isLogScale: PropTypes.bool,
     cutoff: PropTypes.number,
-    isStacked: PropTypes.bool
   }
 
   state = {
@@ -77,11 +76,11 @@ class Barh extends Component {
     if (this.state.collapsed && cutoff) {
       const visibleRows = data.filter((v, i) => i < cutoff);
       const hiddenRows = data.filter((v, i) => i >= cutoff);
-      
-      return data[0].value !== undefined ? visibleRows.concat({
-          name: 'Otros',
-          value: hiddenRows.reduce((val, row) => val + row.value, 0),
-        }) : visibleRows;
+
+      return visibleRows.concat({
+        name: 'Otros',
+        value: hiddenRows.reduce((val, row) => val + row.value, 0),
+      });
     }
 
     return data;
@@ -99,11 +98,11 @@ class Barh extends Component {
   }
 
   toPercent(decimal, fixed = 0) {
-	  return `${(decimal * 100 * 100).toFixed(fixed)/100}%`;
+    return `${(decimal * 100 * 100).toFixed(fixed) / 100}%`;
   }
 
   toNumber(decimal, fixed = 2) {
-	  return `${(decimal * 100).toFixed(fixed)/100}`;
+    return `${(decimal * 100).toFixed(fixed) / 100}`;
   }
 
   toggleCollapse(e) {
@@ -111,16 +110,40 @@ class Barh extends Component {
     this.setState(({ collapsed }) => ({ collapsed: !collapsed }));
   }
 
+  renderTooltip(props) {
+    const { payload: dimensions, active, label, formatter } = props;
+
+    if (!active) {
+      return null;
+    }
+
+    if (dimensions.length === 1) {
+      return <p>{`${label}: ${formatter(dimensions[0].value)}`}</p>;
+    }
+
+    return (
+      <ul className='tooltip'>
+        {dimensions.map((payload, idx) =>
+          <li className='tooltip-item' key={idx} style={{ color: payload.color }}>
+            <span className='name'>{`${payload.dataKey}:`}</span>
+            <span className='value'>{`${payload.value}`}</span>
+          </li>)}
+      </ul>
+    );
+  }
+
+
   render() {
-    const { isPercentual = false, isLogScale = false, cutoff = 0, minLogScale = 0.01, isStacked = false } = this.props;
+    const { isPercentual = false, isLogScale = false, cutoff = 0 } = this.props;
     const { collapsed } = this.state;
     const data = this.getData();
     const dataKeys = this.getDataKeys();
+    const isStacked = !dataKeys.includes('value');
     const rowCount = this.state.collapsed && cutoff ? cutoff : data.length;
-    const logScaleProps = isLogScale ? { scale: 'log', domain: [minLogScale, 'auto'], allowDataOverflow: true } : null;
-    const height = 31 * (rowCount+2) + 20;
-  
-  	return (
+    const logScaleProps = isLogScale ? { scale: 'log', domain: [0.01, 'auto'], allowDataOverflow: true } : null;
+    const height = 31 * (rowCount + 2) + 20;
+
+    return (
       <div>
         {cutoff ? <div className='more-info-wrapper'><a className={cn('more-info-link', collapsed && 'collapsed')} href='#' onClick={(e) => this.toggleCollapse(e)} >{collapsed ? 'ver más' : 'ver menos'}</a></div> : null}
         <BarChart width={620} height={height} data={data}
@@ -130,13 +153,13 @@ class Barh extends Component {
           <CartesianGrid strokeDasharray="2 2" />
           <XAxis type="number" tickFormatter={isPercentual ? this.toPercent : this.toNumber} {...logScaleProps} />
           <YAxis dataKey="name" type="category" width={200} />
-          <Tooltip formatter={isPercentual ? this.toPercent : this.toNumber} />
-          { !dataKeys.includes('value') ? <Legend /> : null }
+          <Tooltip content={this.renderTooltip} formatter={isPercentual ? this.toPercent : this.toNumber} />
+          {isStacked ? <Legend /> : null}
           {dataKeys.map((dataKey, indexGroup) =>
-            <Bar key={dataKey} dataKey={dataKey} stackId={ isStacked ? 'a' : null } fill={this.getDataKeyColor(indexGroup)} >
+            <Bar key={dataKey} dataKey={dataKey} stackId="a" fill={this.getDataKeyColor(indexGroup)} >
               {
                 data.map((entry, indexRow) => (
-                  <Cell cursor="pointer" fill={entry.name === 'Otros' && data[0].value !== undefined ? '#82ca9d' : this.getDataKeyColor(indexGroup)} key={`cell-${indexRow}`} />
+                  <Cell cursor="pointer" fill={entry.name === 'Otros' ? '#82ca9d' : this.getDataKeyColor(indexGroup)} key={`cell-${indexRow}`} />
                 ))
               }
             </Bar>
