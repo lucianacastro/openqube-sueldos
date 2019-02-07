fa8b798ae8717abaa299428bd9fe5a8d6fd3eb73
Luciana Castro <lucianaycastro@gmail.com>
2019-02-06 23:00:38 -0300

    Adding color const in chartsUtils.

--- src/components/Section/Components/Area/index.js
+++ src/components/Section/Components/Area/index.js
@@ -12,42 +12,7 @@ import {
 } from 'recharts';
 
 import './styles.css';
-
-const COLORS = [
-  // OpenQube logo
-  //'#6d71ac',
-  '#9265b3',
-  '#f15b88',
-  '#fea241',
-  
-  // https://www.color-hex.com/color-palette/67140
-  '#8577b6',
-  '#d072cc',
-  '#e5ab83',
-  '#fff2a5',
-  '#856363',
-
-  // https://www.color-hex.com/color-palette/61298 (inverse)
-  '#656abb',
-  '#9b7cc3',
-  '#d28eca',
-  '#e3c2bd',
-  '#f4f6af',
-
-  // https://www.color-hex.com/color-palette/71119
-  '#ffb57c',
-  '#fa6579',
-  '#bb1f8c',
-  '#6a1596',
-  '#18299b',
-
-  // https://www.color-hex.com/color-palette/70941
-  '#b1e2f0',
-  '#f4f4b9',
-  '#fdc268',
-  '#fb9ed6',
-  '#d282e1',
-];
+import { COLORS4 as COLORS } from '../chartsUtils';
 
 class CustomizedLabel extends Component {
   render() {
@@ -70,11 +35,11 @@ class Area extends Component {
   }
 
   toPercent(decimal, fixed = 0) {
-	  return `${(decimal * 100 * 100).toFixed(fixed)/100}%`;
+    return `${(decimal * 100 * 100).toFixed(fixed) / 100}%`;
   }
 
   toNumber(decimal, fixed = 2) {
-	  return `${(decimal * 100).toFixed(fixed)/100}`;
+    return `${(decimal * 100).toFixed(fixed) / 100}`;
   }
 
   getDataKeyColor(index) {
@@ -83,16 +48,16 @@ class Area extends Component {
 
   render() {
     const { data, xDataKey, yDataKeys, isPercentual } = this.props;
-  
-  	return (
-    	<AreaChart  width={620} height={400} data={data}
-      margin={{ top: 10, right: 0, left: 0, bottom: 10 }}
-      className='area-chart'>
-        <CartesianGrid strokeDasharray="3 3"/>
+
+    return (
+      <AreaChart width={620} height={400} data={data}
+        margin={{ top: 10, right: 0, left: 0, bottom: 10 }}
+        className='area-chart'>
+        <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey={xDataKey} />
         <YAxis tickFormatter={isPercentual ? this.toPercent : this.toNumber} />
         <Tooltip formatter={isPercentual ? this.toPercent : this.toNumber} />
-        { !yDataKeys.includes('value') ? <Legend /> : null }
+        {!yDataKeys.includes('value') ? <Legend /> : null}
         {yDataKeys.map((dataKey, i) => (
           <_Area type='monotone' dataKey={dataKey} stroke={COLORS[i]} fill={COLORS[i]} key={`${dataKey}-${i}`} stackId='1' />
         ))}
--- src/components/Section/Components/chartsUtils.js
+++ src/components/Section/Components/chartsUtils.js
@@ -87,3 +87,38 @@ export const COLORS3 = [
     '#d282e1',
 ];
 
+const COLORS4 = [
+    // OpenQube logo
+    //'#6d71ac',
+    '#9265b3',
+    '#f15b88',
+    '#fea241',
+
+    // https://www.color-hex.com/color-palette/67140
+    '#8577b6',
+    '#d072cc',
+    '#e5ab83',
+    '#fff2a5',
+    '#856363',
+
+    // https://www.color-hex.com/color-palette/61298 (inverse)
+    '#656abb',
+    '#9b7cc3',
+    '#d28eca',
+    '#e3c2bd',
+    '#f4f6af',
+
+    // https://www.color-hex.com/color-palette/71119
+    '#ffb57c',
+    '#fa6579',
+    '#bb1f8c',
+    '#6a1596',
+    '#18299b',
+
+    // https://www.color-hex.com/color-palette/70941
+    '#b1e2f0',
+    '#f4f4b9',
+    '#fdc268',
+    '#fb9ed6',
+    '#d282e1',
+];
