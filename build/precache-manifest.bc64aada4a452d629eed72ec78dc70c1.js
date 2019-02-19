1f3f31b2f4eae60c78772e159b0c5f008da21e0f
Gerardo Bort <gerardobort@gmail.com>
2019-02-19 02:48:16 -0300

    Latest build

---
+++ build/precache-manifest.bc64aada4a452d629eed72ec78dc70c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59dda5664f7e0f1c609a",
+    "url": "/gerardobort/openqube-3/static/js/runtime~main.59dda566.js"
+  },
+  {
+    "revision": "a69ae79c425af0862a77",
+    "url": "/gerardobort/openqube-3/static/js/main.a69ae79c.chunk.js"
+  },
+  {
+    "revision": "aacd98fb5023fdae5d8f",
+    "url": "/gerardobort/openqube-3/static/js/1.aacd98fb.chunk.js"
+  },
+  {
+    "revision": "a69ae79c425af0862a77",
+    "url": "/gerardobort/openqube-3/static/css/main.81cdcc3f.chunk.css"
+  },
+  {
+    "revision": "98d5d547da58a9cdf115c318749669f4",
+    "url": "/gerardobort/openqube-3/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74c1b64a8cff21befdc7b501ac68555c.js"
+  "/gerardobort/openqube-3/precache-manifest.bc64aada4a452d629eed72ec78dc70c1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/gerardobort/openqube-3/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
