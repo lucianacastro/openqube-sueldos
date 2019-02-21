d7fa759ca799f9649f2131dbf01a9c35c7e2538e
Luciana Castro <lucianaycastro@gmail.com>
2019-02-20 23:51:28 -0300

    Adding SocialList in Header.

--- src/components/SocialList/index.js
+++ src/components/SocialList/index.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+import './styles.css';
 
 export default class SocialList extends Component {
   static displayName = "SocialList";
 
-  static propTypes = {
-  }
-  static defaultProps = {
-  }
-
   render() {
-
+    const url = window.location.href;
+    const text = encodeURI(`Estos son los resultados de la última encuesta de sueldos de @sysarmy! ${url}`);
+    const via = encodeURI('openqube');
+    const hashtags = encodeURI('encuentasIT');
     return (
       <ul className='social-list'>
         <li className='social-item twitter'>
-          <a className='social-item-link'>
-            <FontAwesomeIcon icon={['fab', 'apple']} />
-          </a></li>
+          <a href={`https://twitter.com/intent/tweet?text=${text}&via=${via}&hashtags=${hashtags}`}
+            className='sharing-twitter'
+            target='_blank'
+            rel='noopener noreferrer'
+
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+
+        </li>
+
+
         <li className='social-item facebook'>
-          <a className='social-item-link'>
-            <FontAwesomeIcon icon={['fab', 'apple']} />
+          <a href='https://www.facebook.com/openqube.io/' className='social-item-link' target='_blank' rel='noopener noreferrer'>
+            <FontAwesomeIcon icon={faFacebookF} />
+          </a>
+        </li>
+        <li className='social-item instagram'>
+          <a href='https://www.instagram.com/openqube/' className='social-item-link' target='_blank' rel='noopener noreferrer'>
+            <FontAwesomeIcon icon={faInstagram} />
           </a>
         </li>
       </ul>
